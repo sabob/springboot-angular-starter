c3f6d0ef6b1017f5a4e0ab08a3c29e5b051f81d5
Bob Schellink <sabob1@gmail.com>
2021-08-23 19:00:08 +0200

    add login page

--- frontend/src/app/route/auth.guard.ts
+++ frontend/src/app/route/auth.guard.ts
@@ -1,30 +1,34 @@
-import { Injectable } from '@angular/core';
-import {
-  CanActivate,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-  Router
-} from '@angular/router';
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
 import appUtils from '@app/utils/appUtils';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+  }
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
 
     if (appUtils.isAuthenticated()) {
-      console.log("you are authenticated");
       return true;
 
     } else {
-      console.error("you are not authenticated");
-      const urlTree = this.router.parseUrl('login');
-      urlTree.queryParams['error'] = 'not-authenticated';
-      urlTree.queryParams['next'] = state.url;
-      return urlTree;
+      return this.handleAccess(state);
     }
   }
+
+  handleAccess(state: RouterStateSnapshot) {
+
+    console.error("you are not authenticated");
+    const urlTree = this.router.parseUrl('login');
+
+    if (state.url) {
+      urlTree.queryParams['error'] = 'not-authenticated';
+      urlTree.queryParams['next'] = state.url;
+    }
+    return urlTree;
+  }
 }
+
